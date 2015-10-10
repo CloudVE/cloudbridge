fa74704c890dd479d72628fb59a251510bb4ad90
Enis Afgan <afgane@gmail.com>
2015-10-10 08:24:24 -0400

    Add a tests for security services

--- test/test_provider_security_service.py
+++ test/test_provider_security_service.py
@@ -1,4 +1,6 @@
-from cloudbridge.providers import interfaces
+import uuid
+
+# from cloudbridge.providers import interfaces
 from test.helpers import ProviderTestBase
 
 
@@ -8,14 +10,49 @@ class ProviderSecurityServiceTestCase(ProviderTestBase):
         super(ProviderSecurityServiceTestCase, self).__init__(
             methodName=methodName, provider=provider)
 
-    def test_list_key_pairs(self):
-        key_pairs = self.provider.security.key_pairs.list()
-        # Assume there's always one keypair at least
-        self.assertIsInstance(key_pairs[0], interfaces.KeyPair)
-        self.assertIsNotNone(key_pairs[0].name)
+    # def test_list_key_pairs(self):
+    #     key_pairs = self.provider.security.key_pairs.list()
+    #     # Assume there's always one keypair at least
+    #     self.assertIsInstance(key_pairs[0], interfaces.KeyPair)
+    #     self.assertIsNotNone(key_pairs[0].name)
 
-    def test_list_security_groups(self):
-        groups = self.provider.security.security_groups.list()
-        # Assume there's always one keypair at least
-        self.assertIsInstance(groups[0], interfaces.SecurityGroup)
-        self.assertIsNotNone(groups[0].name)
+    # def test_list_security_groups(self):
+    #     groups = self.provider.security.security_groups.list()
+    #     # Assume there's always one keypair at least
+    #     self.assertIsInstance(groups[0], interfaces.SecurityGroup)
+    #     self.assertIsNotNone(groups[0].name)
+
+    def test_crud_key_pair_service(self):
+        name = 'cbtestkeypair-{0}'.format(uuid.uuid4())
+        kp = self.provider.security.key_pairs.create(key_name=name)
+        kpl = self.provider.security.key_pairs.list()
+        found_kp = [k for k in kpl if k.name == name]
+        self.assertTrue(
+            len(found_kp) == 1,
+            "List key pairs did not return return expected key {0}."
+            .format(name))
+        self.provider.security.key_pairs.delete(key_name=kp.name)
+        kpl = self.provider.security.key_pairs.list()
+        found_kp = [k for k in kpl if k.name == name]
+        self.assertTrue(
+            len(found_kp) == 0,
+            "Key pair {0} should have been deleted but still exists."
+            .format(name))
+
+    def test_crud_security_group_service(self):
+        name = 'cbtestsecuritygroup-{0}'.format(uuid.uuid4())
+        sg = self.provider.security.security_groups.create(
+            name=name, description=name)
+        sgl = self.provider.security.security_groups.get(group_names=[sg.name])
+        found_sg = [g for g in sgl if g.name == name]
+        self.assertTrue(
+            len(found_sg) == 1,
+            "List security groups did not return return expected group {0}."
+            .format(name))
+        self.provider.security.security_groups.delete(group_id=sg.id)
+        sgl = self.provider.security.security_groups.list()
+        found_sg = [g for g in sgl if g.name == name]
+        self.assertTrue(
+            len(found_sg) == 0,
+            "Security group {0} should have been deleted but still exists."
+            .format(name))
