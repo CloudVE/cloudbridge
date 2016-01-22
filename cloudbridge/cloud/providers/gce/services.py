e90a7f6885814a3477cd0b38398d62af64f91093
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-01-22 17:40:39 +1100

    Added keypair and security service stubs.

--- cloudbridge/cloud/providers/gce/services.py
+++ cloudbridge/cloud/providers/gce/services.py
@@ -1,3 +1,5 @@
+from cloudbridge.cloud.base.services import BaseKeyPairService
+from cloudbridge.cloud.base.services import BaseSecurityGroupService
 from cloudbridge.cloud.base.services import BaseSecurityService
 
 
@@ -5,3 +7,27 @@ class GCESecurityService(BaseSecurityService):
 
     def __init__(self, provider):
         super(GCESecurityService, self).__init__(provider)
+
+        # Initialize provider services
+        self._key_pairs = GCEKeyPairService(provider)
+        self._security_groups = GCESecurityGroupService(provider)
+
+    @property
+    def key_pairs(self):
+        return self._key_pairs
+
+    @property
+    def security_groups(self):
+        return self._security_groups
+
+
+class GCEKeyPairService(BaseKeyPairService):
+
+    def __init__(self, provider):
+        super(GCEKeyPairService, self).__init__(provider)
+
+
+class GCESecurityGroupService(BaseSecurityGroupService):
+
+    def __init__(self, provider):
+        super(GCESecurityGroupService, self).__init__(provider)
