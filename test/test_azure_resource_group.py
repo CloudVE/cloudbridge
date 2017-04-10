986b52cb303e96daedcbd7faa6a5d08eb8185db5
jatin <jatin.kinra@appliedis.com>
2017-04-10 15:58:09 +0530

    Resource Group file added. Issue for RG tests resolved.

---
+++ test/test_azure_resource_group.py
@@ -0,0 +1,29 @@
+import json
+import unittest
+import uuid
+
+from cloudbridge.cloud.interfaces import TestMockHelperMixin
+
+from test.helpers import ProviderTestBase
+import test.helpers as helpers
+
+
+class AzureResourceGroupTestCase(ProviderTestBase):
+    def __init__(self, methodName, provider):
+        super(AzureResourceGroupTestCase, self).__init__(
+            methodName=methodName, provider=provider)
+
+    def test_resource_group_create(self):
+        resource_group_params = {'location': self.provider.region_name}
+        rg = self.provider._azure_client.create_resource_group(self.provider.resource_group, resource_group_params)
+        print("Create Resource - " + str(rg))
+        self.assertTrue(
+            rg.name == "cloudbridge-azure",
+            "Resource Group should be Cloudbridge")
+
+    def test_resource_group_get(self):
+        rg = self.provider._azure_client.get_resource_group(self.provider.resource_group)
+        print("Get Resource - " + str(rg))
+        self.assertTrue(
+            rg.name == "testResourceGroup",
+            "Resource Group should be Cloudbridge")
