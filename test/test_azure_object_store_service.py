f9ca56aa05843811fc0a09084c7d65cbd0023a2e
jatin <jatin.kinra@appliedis.com>
2017-04-12 10:55:50 +0530

    Tests for Find Bucket added

--- test/test_azure_object_store_service.py
+++ test/test_azure_object_store_service.py
@@ -22,13 +22,30 @@ class AzureObjectStoreServiceTestCase(ProviderTestBase):
             "Object create returned value should be None")
 
     @helpers.skipIfNoService(['object_store'])
-    def test_azure_bucket_find(self):
+    def test_azure_bucket_find_Exist(self):
         container = self.provider.object_store.find("container1")
-        print(str(container))
-        self.assertTrue(
-            str(container) == "[<CB-AzureBucket: container1>]",
-            "Object find returned value should be container1")
+        print("Find Exist - " + str(container))
+        self.assertEqual(
+            len(container) ,1)
 
     @helpers.skipIfNoService(['object_store'])
-    def test_azure_bucket_get(self):
-        pass
+    def test_azure_bucket_find_NotExist(self):
+        container = self.provider.object_store.find("container3")
+        print("Find Not Exist - " + str(container))
+        self.assertEqual(
+            len(container), 0)
+
+    @helpers.skipIfNoService(['object_store'])
+    def test_azure_bucket_get_Exist(self):
+        container = self.provider.object_store.get("container2")
+        print("Get Exist - " + str(container))
+        self.assertTrue(
+            str(container) == "<CB-AzureBucket: container2>",
+            "Object find returned value should be container3")
+
+    @helpers.skipIfNoService(['object_store'])
+    def test_azure_bucket_get_NotExist(self):
+        container = self.provider.object_store.get("container3")
+        print("Get Not Exist - " + str(container))
+        self.assertEqual(
+            str(container) , 'None')
