154d210f7f39ff35da1e05c986a5f2133af537d3
chandraMsekhar <chandrasekhar.madduri@appliedis.com>
2017-05-24 10:45:03 +0530

    Updated Setup for build issues again

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join('cloudbridge', '__init__.py')) as f:
             version = ast.literal_eval(m.group(1))
             break
 
-base_reqs = ['requests==2.12.5', 'bunch>=1.0.1',
+base_reqs = ['requests==2.14.2', 'bunch>=1.0.1',
              'six>=1.10.0', 'retrying>=1.3.3']
 openstack_reqs = ['Babel==2.3.4',
                   'python-novaclient==7.0.0',
