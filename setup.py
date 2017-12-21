88200788aff86a759e50fad71d493204434a59ea
Enis Afgan <afgane@gmail.com>
2017-12-21 01:46:46 -0500

    Use moto fork
    Until https://github.com/spulec/moto/issues/1396 is resolved

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ REQS_FULL = REQS_BASE + REQS_AWS + REQS_AZURE + REQS_OPENSTACK
 # httpretty is required with/for moto 1.0.0 or AWS tests fail
 REQS_DEV = ([
     'tox>=2.1.1',
-    'moto>=1.1.11',
+    # 'moto>=1.1.11',  # until https://github.com/spulec/moto/issues/1396
     'sphinx>=1.3.1',
     'flake8>=3.3.0',
     'flake8-import-order>=0.12'] + REQS_FULL
@@ -58,7 +58,7 @@ setup(
     description='A simple layer of abstraction over multiple cloud providers.',
     author='Galaxy and GVL Projects',
     author_email='help@genome.edu.au',
-    url='http://cloudbridge.readthedocs.org/',
+    url='http://cloudbridge.cloudve.org/',
     install_requires=REQS_FULL,
     extras_require={
         ':python_version=="2.7"': ['py2-ipaddress'],
