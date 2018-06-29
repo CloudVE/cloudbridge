fae6208db4ba772585816c6a4ee03918cf3a5f2e
Alexandru Mahmoud <amahmou4@jhu.edu>
2018-06-29 11:19:03 -0700

    Azure with gallery images

--- cloudbridge/cloud/providers/azure/resources.py
+++ cloudbridge/cloud/providers/azure/resources.py
@@ -681,7 +681,7 @@ class AzureMachineImage(BaseMachineImage):
         else:
             self._state = 'AVAILABLE'
 
-        if not isinstance(image, dict) and not self._image.tags:
+        if not isinstance(self._image, dict) and not self._image.tags:
             self._image.tags = {}
 
     @property
@@ -793,6 +793,13 @@ class AzureMachineImage(BaseMachineImage):
                 log.exception(cloudError.message)
                 # image no longer exists
                 self._state = "unknown"
+
+    def isgalleryimage(self):
+        """
+        Returns true if the image is a public reference and false if it
+        is a private image in the resource group.
+        """
+        return isinstance(self._image, dict)
 
 
 class AzureGatewayContainer(BaseGatewayContainer):
