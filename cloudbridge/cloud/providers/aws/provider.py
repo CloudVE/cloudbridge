489b6189445efc2cb374688510283af8b4eafedb
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-01-29 20:53:35 +0530

    Debug commit for AWS issues on Travis

--- cloudbridge/cloud/providers/aws/provider.py
+++ cloudbridge/cloud/providers/aws/provider.py
@@ -3,13 +3,11 @@ import logging as log
 
 import boto3
 
-try:
-    # These are installed only for the case of a dev instance
-    import responses
-    from moto import mock_ec2
-    from moto import mock_s3
-except ImportError:
-    log.debug('[aws provider] moto library not available!')
+# These are installed only for the case of a dev instance
+from moto import mock_ec2
+from moto import mock_s3
+
+import responses
 
 from cloudbridge.cloud.base import BaseCloudProvider
 from cloudbridge.cloud.base.helpers import get_env
