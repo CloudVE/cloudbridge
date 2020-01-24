1611f8795d953262f7756ff7693b241e19b7d56b
Enis Afgan <afgane@gmail.com>
2020-01-24 17:11:51 -0500

    Update acceptable tenacity versions
    Removes a conflict with CloudLaunch:
    https://github.com/galaxyproject/cloudlaunch/blob/f3c083ad0ea47ffab145d1ae4796ec130daa1f8d/setup.py#L95

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join('cloudbridge', '__init__.py')) as f:
 
 REQS_BASE = [
     'six>=1.11',
-    'tenacity>=4.12.0,<=5.0',
+    'tenacity>=6.0',
     'cachetools>=2.1.0',
     'deprecation>=2.0.7',
     'pyeventsystem<2'
